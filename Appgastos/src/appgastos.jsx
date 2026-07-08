143ac0797afc8ec2a8f93be9204799b69103422a
Vibort <vibort1@gmail.com>
2026-07-08 17:31:28 -0300

    Update appgastos.jsx

--- Appgastos/src/appgastos.jsx
+++ Appgastos/src/appgastos.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import {
   PieChart, Pie, Cell, Tooltip, Legend, ResponsiveContainer,
   BarChart, Bar, XAxis, YAxis, CartesianGrid,
